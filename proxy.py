d88b2b23c3e8b07ba5c998078b7aa88ac296f9e5
Mikhail Efimov <efimovhq@gmail.com>
2022-05-26 23:08:34 +0400

    Compatability fix for Firefox and Safari

--- proxy.py
+++ proxy.py
@@ -1,131 +1,79 @@
-from ctypes import sizeof
-import socketserver
 import http.server
 import re
 from os.path import exists
 import mimetypes
+from cgi import parse_header
+from sys import getsizeof
+from http.server import ThreadingHTTPServer
 import httplib2
 import lxml.html
-import requests
-from sys import getsizeof
-from http import HTTPStatus
-from http.server import ThreadingHTTPServer
 
 #httplib2.debuglevel = 0
-h = httplib2.Http('.cache')
 PORT = 8000
 ADDRESS = "https://news.ycombinator.com/"
 
 
 class Proxy(http.server.SimpleHTTPRequestHandler):
+    _h = httplib2.Http('.cache')
+
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
+        #self.protocol_version = "HTTP/1.1"
         if self.path.endswith("gif") or self.path.endswith("ico"):
             imgname = self.path[1:]
             if not exists(imgname):
                 self.save_remote_image(imgname)
+            self.send_response(200)
             self.get_local_image(imgname)
             return
-        if self.path.endswith("aLT"):
-            imgname = self.path[1:]
-            if not exists(imgname):
-                self.save_remote_image(imgname)
-        #self.log_headers()
-
         url = ADDRESS + self.path[1:]
-        response, content = h.request(url)
-        # new_request = requests.get(url, headers = self.trim_header(self.headers))
-        # self.apply_headers(new_request.headers, False)
-        # #self.log_headers([new_request.headers, new_request.request.headers])
-        # with open("header1.text", "w") as f:
-        #     for key in new_request.headers:
-        #         f.write(str(key) + ": " + new_request.headers[key])
-        #         f.write("\n")
-        # with open("header2.text", "w") as f:
-        #     for key in new_request.request.headers:
-        #         f.write(str(key) + ": " + new_request.request.headers[key])
-        #         f.write("\n")
-        # self.wfile.write(new_request.content)
-        # return
-        # self.log_headers(response)
-        
-        body = content.decode()
-        doc = lxml.html.fromstring(body) # bytes btw KEKW
-        doc = self.modify_html(doc)
-        # result = lxml.html.tostring(doc, encoding="unicode") # this is actually bytes
-        result = lxml.html.tostring(doc)
-        print((str(type(result))).capitalize())
-        print((str(type(result))).capitalize())
-        print((str(type(result))).capitalize())
-        self.log_into_file(result, "w")
-        # with open("page.html", "wb") as f:
-        #     f.write(result)
-        # print(body == result)
-        self.log_into_file(body, name="body")
-        self.log_into_file(content, name="content")
-        self.log_into_file(result, name="result")
-        self.log_headers([response])
-        x = self.wfile
-        self.wfile.flush()
-        print(x == self.wfile)
-        self.log_into_file(response, name="response1")
-        self.apply_headers(response, result=result)
-        self.log_into_file(response, name="response2")
-       # print(content == bytes(result, 'utf-8'))
-        self.log_into_file(self.headers, name="/Users/Svetlana/Desktop/VSCode/headers5")
-        # print(self.wfile.readable())
-        # self.log_into_file(self.wfile.read(), name="wfile_original", mode="wb")
-        # logging.basicConfig(filename='loglog.log', encoding='utf-8', level=logging.DEBUG, filemode="w")
-        # logging.debug(content)
-        # logging.debug(result)
-        print("size = " + str(getsizeof(result)))
-        self.wfile.write(result)
+        response, content = self.make_request(url)
+        types, _ = parse_header(response["content-type"])
+        if "html" in types.split("/"):
+            doc = lxml.html.fromstring(content.decode())
+            doc = self.modify_html(lxml.html.fromstring(content.decode()))
+            content = lxml.html.tostring(doc) # bytes btw KEKW
+        self.apply_headers(response, length=getsizeof(content))
+        self.wfile.write(content)
 
-    def apply_headers(self, headers, end_header=True, result=None):
+    def apply_headers(self, headers, end_header=True, length=None):
         for key in headers:
-            if key.lower() == "content-length":         
-                self.send_header("content-length", str(getsizeof(result) - 33))
+            if key.lower() == "transfer-encoding":
+                continue
+            if key.lower() == "content-length":
+                self.send_header("content-length", str(length - 33))
                 continue
             self.send_header(key.lower(), headers[key])
-        print(self._headers_buffer)
         if end_header:
             self.end_headers()
-    
-    def log_into_file(self, x, mode="w", name="log"):
-        with open(name + ".text", mode) as f:
+
+    def make_request(self, url):
+        response, content = self._h.request(url)
+        self.send_response(response.status)
+        return response, content
+
+    def log_into_file(self, entity, mode="w", name="log"):
+        with open(name + ".text", mode) as file:
             if mode == "w":
-                x = str(x)
-            f.write(x)
-            f.close()
+                entity = str(entity)
+            file.write(entity)
+            file.close()
 
     def log_headers(self, headers):
         i=1
-        for header in headers:             
-            with open("header{}.text".format(str(i)), "w") as f:
+        for header in headers:
+            with open("header{}.text".format(str(i)), "w") as file:
                 for key in header:
-                    f.write(str(key) + ": " + header[key])
-                    f.write("\n")
-            # f.writelines(str(i))
-            # f.writelines(str("  "))
-            # f.writelines(str(self.headers[i]) + "\n")
-    
+                    file.write(str(key) + ": " + header[key])
+                    file.write("\n")
+
     def save_remote_image(self, imgname):
-        response, remote_file = h.request(ADDRESS + imgname)
+        response, remote_file = self.make_request(ADDRESS + imgname)
         for i in ["last-modified", "cache-control", "etag", "expires", "accept-ranges", "status"]:
             if i in response:
                 self.send_header(i, response[i])
         #self.apply_headers(response)
         with open(imgname, 'wb') as local_file:
             local_file.write(remote_file)
-    
-    def trim_header(self, header):
-        whitelist = ["connection", "cache-control", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform",
-        "User-Agent", "Accept-Encoding"]
-        trimmed = {}
-        for key in header:
-            if key.lower() in whitelist:
-                trimmed[key.lower()] = header[key]
-        return trimmed
 
     def get_local_image(self, imgname):
         with open(imgname, 'rb') as local_file:
@@ -143,11 +91,7 @@ class Proxy(http.server.SimpleHTTPRequestHandler):
             node.set("href", "")
         return tree
 
-# class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-#     def __init__(self, server_address, HandlerClass):
-#         HTTPServer.__init__(self, server_address, HandlerClass)
 
-httpd = socketserver.ForkingTCPServer(('', PORT), Proxy)
+httpd = ThreadingHTTPServer(('', PORT), Proxy)
 print("Now serving at " + str(PORT))
 httpd.serve_forever()
-
